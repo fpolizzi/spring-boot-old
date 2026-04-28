546e671158166bece3b550700c91bd66d5c3392b
fpolizzi <39471219+fpolizzi@users.noreply.github.com>
2026-04-28 21:04:33 +0200

    integrate spring-framework into getting-started (#1)
    * build: bump spring-boot version to 4.0.6
    
    * refactor: switch package names
    
    * code
    
    * feat: create context variable to get more information about the spring context
    
    * feat: get all beans in the container
    
    * feat: get number of beans in the container
    
    * feat: add user defined bean redBean
    
    * feat: add commandLineRunner
    
    * feat: example for dependency injection
    
    * feat: make dependency injection more obvious
    
    * feat: use two user-defined beans
    
    * feat: add record User
    
    * feat: add component UserService
    
    * feat: add component UserService
    
    * feat: invoke getUsers in commandLineRunner
    
    * feat: add getUserById
    
    * feat: invoke getUserById in commandLineRunner
    
    * Update course link in README
    
    * feat: use @Service as a more specific annotation
    
    * feat: add @PostConstruct method
    
    * feat: add @PreDestroy method
    
    * feat: add constructor to UserService
    
    ---------
    
    Co-authored-by: amigoscode <amigoscode@gmail.com>

---
+++ src/main/java/com/fpolizzi/SpringAndSpringBootApplication.java
@@ -0,0 +1,107 @@
+package com.fpolizzi;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootApplication
+public class SpringAndSpringBootApplication {
+
+    static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(
+                SpringAndSpringBootApplication.class,
+                args
+        );
+
+        // print out all beans that are in the spring container
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            // System.out.println(beanDefinitionName);
+        }
+
+        // total number of beans in the spring container
+        System.out.println("\nNumber of Beans: " + beanDefinitionNames.length);
+    }
+
+    // user-defined bean
+    @Bean
+    public String redBean() {
+
+        return "Manchester United";
+    }
+
+    @Bean
+    public String blueBean() {
+
+        return "Chelsea";
+    }
+
+    // execute CommandLineRunner
+    @Bean
+    CommandLineRunner commandLineRunner(String redBean,
+                                        String blueBean,
+                                        UserService userService) {
+
+        return args -> {
+
+            System.out.println("\nHello from CommandLineRunner");
+            System.out.println(redBean());
+            System.out.println(blueBean());
+            System.out.println(userService.getUsers());
+            System.out.println(userService.getUserById(1));
+            System.out.println(userService.getUserById(3));
+            System.out.println();
+        };
+    }
+
+    public record User(
+            int id,
+            String name
+    ) {
+    }
+
+    @Service
+    public class UserService {
+
+        public UserService() {
+
+            System.out.println("UserService Constructor");
+        }
+
+        @PostConstruct
+        public void init() {
+
+            System.out.println("Before bean creation..");
+        }
+
+        @PreDestroy
+        public void tearDown() {
+
+            System.out.println("Before bean destroying..");
+        }
+
+
+        public List<User> getUsers() {
+
+            return List.of(
+                    new User(1, "John Doe"),
+                    new User(2, "Jane Doe")
+            );
+        }
+
+        public Optional<User> getUserById(int id) {
+
+            return getUsers().stream()
+                    .filter(user -> user.id == id)
+                    .findFirst();
+        }
+    }
+}
